9e33532463e1cf0e2af0f08c9692d67bc622e028
M-auro <mauromorrati@outlook.com>
2025-02-16 11:49:28 +0000

    add response for 'Hi' input in lambda function

--- function/lambda_function.py
+++ function/lambda_function.py
@@ -11,6 +11,8 @@ def lambda_handler(event, context):
     print('Starting functions\n---------------------------------------------')
 
     if event["input"] == "Hello":
+        return "World"    
+    if event["input"] == "Hi":
         return "World"
     else:
         raise ValueError("Invalid input")
